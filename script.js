00e223c82163055bfbae5f75d955612ab33e4464
davihm <122923127+davihm@users.noreply.github.com>
2024-08-24 13:41:40 +0000

    exercicio 2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function livro(nome, ano, autor) {
-    const nomeMaisculo = nome.toUpperCase()
-    const anoDesdeLancamento = 2024-ano
-    const frase = `${nome} por ${autor}`;
-    return {
-        nomeMaisculo,
-        anoDesdeLancamento,
-        frase
-    }
+// function livro(nome, ano, autor) {
+//     const nomeMaisculo = nome.toUpperCase()
+//     const anoDesdeLancamento = 2024-ano
+//     const frase = `${nome} por ${autor}`;
+//     return {
+//         nomeMaisculo,
+//         anoDesdeLancamento,
+//         frase
+//     }
+// }
+// const senhorDosAneis = livro('O Senhor dos Anéis', 1954, 'J. R. R. Tolkien')
+// const harryPotter = livro('Harry Potter', 1997, 'J. K. Rowling')
+// console.log(senhorDosAneis, harryPotter)
+
+
+const circulo = document.querySelector(".circulo")
+
+function moverMouse(event) {
+    const larguraCirculo = circulo.offsetWidth
+    const alturaCirculo = circulo.offsetHeight 
+    const valorTop = event.clientY - (alturaCirculo/2)
+    const valorLeft = event.clientX - (larguraCirculo/2)
+    circulo.style.top = `${valorTop}px`
+    circulo.style.left = `${valorLeft}px`
 }
-const senhorDosAneis = livro('O Senhor dos Anéis', 1954, 'J. R. R. Tolkien')
-const harryPotter = livro('Harry Potter', 1997, 'J. K. Rowling')
-console.log(senhorDosAneis, harryPotter)
+
+window.addEventListener('mousemove', moverMouse)
