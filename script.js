a0b14457ddd5e3c8961bb6d21e2020d54b060d24
davihm <122923127+davihm@users.noreply.github.com>
2024-08-24 14:56:44 +0000

    exercicio 3

--- script.js
+++ script.js
@@ -13,15 +13,28 @@
 // console.log(senhorDosAneis, harryPotter)
 
 
-const circulo = document.querySelector(".circulo")
+// const circulo = document.querySelector(".circulo")
 
-function moverMouse(event) {
-    const larguraCirculo = circulo.offsetWidth
-    const alturaCirculo = circulo.offsetHeight 
-    const valorTop = event.clientY - (alturaCirculo/2)
-    const valorLeft = event.clientX - (larguraCirculo/2)
-    circulo.style.top = `${valorTop}px`
-    circulo.style.left = `${valorLeft}px`
-}
+// function moverMouse(event) {
+//     const larguraCirculo = circulo.offsetWidth
+//     const alturaCirculo = circulo.offsetHeight 
+//     const valorTop = event.clientY - (alturaCirculo/2)
+//     const valorLeft = event.clientX - (larguraCirculo/2)
+//     circulo.style.top = `${valorTop}px`
+//     circulo.style.left = `${valorLeft}px`
+// }
 
-window.addEventListener('mousemove', moverMouse)
+// window.addEventListener('mousemove', moverMouse)
+
+const botao = document.querySelector(".botao")
+const valor = document.querySelector("div")
+if (botao){
+    function somar(event) {
+        if (Number(valor.innerHTML) < 10) {
+        valor.innerHTML = Number(valor.innerHTML) + 1;
+        } else {
+            console.error("NÃO É POSSÍVEL ADICIONAR MAIS!! VOCÊ JÁ CLICOU 10 VEZES")
+        }
+    }
+    botao.addEventListener('click', somar)   
+} 
